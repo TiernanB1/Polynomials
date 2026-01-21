f419323aecf27462042ad93177d941d40ee2ec90
Tiernan Blain <s2729376@ed.ac.uk>
2026-01-21 16:32:01 +0000

    Print function implemented

--- Polynomials.py
+++ Polynomials.py
@@ -1,12 +1,31 @@
 import math 
 
-class poly: 
-    pass
+class Poly: 
 
-def main():
+    def __init__(self, coeffs1):
+        self.coeffs = coeffs1
+        
+
+ #   def __str__(self):
+  #      for i in range(len(self.coeffs)):
+   #         return f"({self.coeffs[i]}x^{i})"
+
+    def output(self):
+        for i in range(len(self.coeffs)):
+           (f"({self.coeffs[i]}x^{i})")
+                 
+
+if __name__ == "__main__":
     
-    coeffs = input("enter values of coefficients").split() # get coefficients from input 
+    pa = Poly([1, 2, 4, 7])
 
+    Poly.output(pa)
 
-main()
+#"coeffs1 = Poly(input("enter values of coefficients").split()) # get coefficients from input "
+    
+# print(coeffs1)"
+# coeffs1 = Poly(input("enter values of coefficients").split()) # get coefficients from input 
+    
+# print("why")
+
 
