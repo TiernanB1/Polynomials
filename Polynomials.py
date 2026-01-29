4272654bded5eae0511e336ff198f977309d7a57
Tiernan Blain <s2729376@ed.ac.uk>
2026-01-29 15:30:45 +0000

    Print function updated to handle edge cases

--- Polynomials.py
+++ Polynomials.py
@@ -2,24 +2,36 @@
 
 class Poly: 
 
-    def __init__(self, coeffs1, c = 2):
+    def __init__(self, coeffs1):
         self.coeffs = coeffs1
-        self.c = c
         
 
     def __str__(self):
-       
-       output = [str(self.coeffs[0])] # add first item on its own 
-       
-       for i in range(1, len(self.coeffs)): #loop to end of the list 
+
+      output = ""
+
+      if self.coeffs[0] != 0: 
+         output = str(self.coeffs[0]) # add first item on its own 
+
+
+      if self.coeffs[1] != 0: 
+         if self.coeffs[1] < 0: 
+            output +=(f" - {-self.coeffs[1]}x")
+         elif self.coeffs[1] > 0:
+            output +=(f" + {self.coeffs[1]}x")  # add first item on its own 
+
+      if self.coeffs[0] == 0: #decide if + is needed or not
+         output=output.replace(" + ","") #replace + if not needed
+
+      for i in range(2, len(self.coeffs)): #loop to end of the list 
          if self.coeffs[i] < 0: # decide if negative or positive sign
-            output.append(f"- {-self.coeffs[i]}x^{i}")
+            output +=(f" - {-self.coeffs[i]}x^{i}")
          elif self.coeffs[i] > 0:
-            output.append(f"+ {self.coeffs[i]}x^{i}")
+            output +=(f" + {self.coeffs[i]}x^{i}")     
          else: 
             pass #skip 0 terms
-         
-       return " ".join(output) # join into string
+      
+      return (output) # join into string
     
     def __add__(self, poly_b):
        return self.add(poly_b)
@@ -35,17 +47,16 @@ class Poly:
           new_poly.append(a + b) # append
        return Poly(new_poly)
     
-    def dif(self):
-       for i in range(1, len(self.coeffs)):
-          self.coeffs[i] = self.coeffs[i] * i
-       self.c = self.coeffs[0]
-       del(self.coeffs[0])
+    def dif(self): 
+       for i in range(len(self.coeffs)): #loops to end of list
+          self.coeffs[i] = self.coeffs[i] * i #multiplies by index
+       del self.coeffs[0] #removed 0 coefficient at the start
        return Poly(self.coeffs)
     
     def antidif(self):
-       new_coeff = [self.c] + self.coeffs
-       for i in range(1, len(new_coeff)):
-          new_coeff[i] = (self.coeffs[i - 1]/ i )
+       new_coeff = [2] + self.coeffs #constant of integration = 2 
+       for i in range(1, len(new_coeff)):   # for range of rest of coefficients
+          new_coeff[i] = (self.coeffs[i - 1]/ i ) # move down place and divide by old coefficient 
        return Poly(new_coeff)
        
 
@@ -53,15 +64,17 @@ class Poly:
 if __name__ == "__main__":
     
     
-    pa = Poly(list(map(int, input("enter coefficients of polynomial a: ").split())))
-    pb = Poly(list(map(int, input("enter coefficients of polynomial b: ").split())))
-   # pa = Poly([1, 2, 3, 4])
-    #pb = Poly([4, -2, 2])
+    #pa = Poly(list(map(int, input("enter coefficients of polynomial a: ").split())))
+    #pb = Poly(list(map(int, input("enter coefficients of polynomial b: ").split())))
+    pa = Poly([2, 0, 4, -1, 0, 6])
+    pb = Poly([-1, -3, 0, 4.5])
     pc = (pa + pb) 
     print(pc)
-    pd = pc.dif()
-    pd = pc.antidif()
+    pd = pa.dif()
     print(pd)
+    pe = pd.antidif()
+    print(pe)
+   
    
     
 
